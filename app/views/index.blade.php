3538ad0b47b1135b52630aff5e5b11125ae3d46a
Luisana <luisana.martin@gmail.com>
2013-10-17 17:20:21 -0500

    over de pantalla inicial eliminado

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,5 +3,4 @@
 	<div id="like">
 		<a href="{{ URL::action('HomeController@premio') }}" id="facebook-like"><img src="imgs/like-btn.png"></a>
 	</div>
-	<div id="over"><a href=""><img src="imgs/top.png"></a></div>
 @stop
